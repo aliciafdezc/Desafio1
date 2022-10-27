11fbb7f12a5bd3103a40cfa9d7c7490737882834
aliciafdezc <cfdezalicia@gmail.com>
2022-10-27 12:29:18 +0200

    modal design

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './styles.css';
 
 import { PanelList } from './panels';
 import { initPanels } from './panels/panelsLogic';
-
+import { initModal } from './panels/tasksModal/modalLogic'
 
 export const panelList = new PanelList();
 panelList.panel.forEach(initPanels); 
+initModal();
--- src/panels/tasksModal/modalLogic.js
+++ src/panels/tasksModal/modalLogic.js
@@ -1,8 +1,18 @@
-const modalContainer = document.querySelectorAll('.modalContainer');
+/* const initModal = () => {
 
-modalContainer.addEventListener('click', (e) => {
-    if (e.target.classList.contains('close')) {
-        document.querySelector('.tasksModal').classList.remove('visible');
-    }
-    
-});
+} */
+export const initModal = () => {
+    const modalContainer = document.querySelector('.modalContainer');
+    modalContainer.addEventListener('click', (e) => {
+        // CLOSE MODAL
+        if (e.target.parentNode.classList.contains('close')) {
+            document.querySelector('.tasksModal').classList.remove('visible');
+        }
+
+        //ADD TASK
+        if (e.target.classList.contains('addTask') || e.target.parentNode.classList.contains('addTask')) {
+            document.querySelector('.tasksModal').classList.remove('visible');
+            
+        }
+    });
+}
