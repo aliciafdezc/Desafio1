69e32c4953e5e0d9455f7b0948c9b8076cc22e9b
aliciafdezc <cfdezalicia@gmail.com>
2022-11-02 09:32:50 +0100

    save task, margin progressbar, background color modal

--- src/js/general.js
+++ src/js/general.js
@@ -2,7 +2,6 @@ export const initChangeTheme = () => {
     const toggle = document.querySelector('footer .toggle span');
     
     toggle.addEventListener('click', (e) => {
-        console.log(toggle)
         const body = document.querySelector('.theme');
         body.classList.toggle('dark');
         body.classList.toggle('light');
